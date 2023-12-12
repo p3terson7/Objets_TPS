e4afa35910c4dacc7804558fc75afb514c002724
p3terson7 <psarateanu@hotmail.ca>
2023-12-12 12:40:56 -0500

    pass data to html & load on refresh

--- Flask/.venv/myapp.py
+++ Flask/.venv/myapp.py
@@ -1,9 +1,19 @@
 # Run: python -m flask --app myapp run
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+data=[
+   {
+      "Temperature":"21.1",
+      "Humidity":"54.1",
+   }
+]
 
 @app.route('/')
-def home():
-   return render_template('index.html')
+def __main__():
+   return render_template('index.html', data=data)
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
